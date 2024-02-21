8e8c5287ca6c63b321ccdb21db992536685af866
8thgencore <axelmaker@ya.ru>
2024-02-21 23:37:57 +0300

    🛠️ Fix database migrations

--- internal/database/migrations/000003_create_secrets_table.up.sql
+++ internal/database/migrations/000003_create_secrets_table.up.sql
@@ -5,7 +5,7 @@ CREATE TYPE "secret_type_enum" AS ENUM ('password', 'text', 'file');
 -- Create the secrets table
 CREATE TABLE IF NOT EXISTS secrets (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    collection_id UUID REFERENCES collections(id),
+    collection_id UUID REFERENCES collections(id) ON DELETE CASCADE, 
     secret_type secret_type_enum NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
